a6e3f80ae6c10380cbd7da6f5a3d6b4c9d6cd392
calbertofilho <84130607+calbertofilho@users.noreply.github.com>
2021-06-11 09:18:04 -0300

    Alterações: 11/06/2021

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ Utilizando a biblioteca: PyGame
 
 Criado por: Carlos Alberto Morais Moura Filho
 Versão: 1.0
-Atualizado em: 10/06/2021
+Atualizado em: 11/06/2021
 '''
 # pylint: disable=no-member
 # pylint: disable=no-name-in-module
